4230bff807c62232a2fb5b09e9b2748b846a0c33
Sebastian6-6 <pedropiassi.business@gmail.com>
2026-03-09 16:46:06 -0300

    Agora sim o glue nao sera apagado

--- glue/scripts/cola.py
+++ glue/scripts/cola.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 # 1. Trava de Segurança para o GitHub Actions
 if os.getenv('GITHUB_ACTIONS') == 'true':
-    print("Validação de sintaxe concluída para a Versão DAORA")
+    print("Validação de sintaxe concluída para a Versão TOP 5.0")
     sys.exit(0)
 
 from awsglue.utils import getResolvedOptions
